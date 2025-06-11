355814d6b072fcd14907fcbec66e693457acb139
rafaelgpaves <rafaelgp3@al.insper.edu.br>
2025-06-10 21:29:01 -0300

    remove: removendo coisas não usadas

--- nos.py
+++ nos.py
@@ -37,114 +37,6 @@ class VarDec(Node):
             tipo, valor, _ = self.children[1].Evaluate(st)
             st.setter(self.children[0].value, (tipo, valor, False))
 
-class FuncDec(Node):
-    def Evaluate(self, st):
-        st.create(self.children[0].value, self.value)
-        st.setter(self.children[0].value, (self.value, self, True))
-
-class FuncCall(Node):
-    def Evaluate(self, st):
-        func = st.getter(self.value)
-        funcdec = func[1]
-        if func[2] is False:
-            raise Exception(f"\"{self.value}\" não é chamável")
-        if len(self.children) != len(funcdec.children) - 2:
-            raise Exception(f"Número de argumentos recebidos: {len(self.children)}; A função \"{self.value}\" esperava {len(funcdec.children) - 2} argumentos")
-        new_st = SymbolTable()
-        new_st.parent_st = st
-
-        for i, arg in enumerate(self.children):
-            tipo, valor, _ = arg.Evaluate(new_st)
-            iden_arg = funcdec.children[i+1].children[0].value
-            tipo_arg = funcdec.children[i+1].value
-            new_st.create(iden_arg, tipo)
-            new_st.setter(iden_arg, (tipo, valor, False))
-
-        retorno_func = funcdec.children[-1].Evaluate(new_st)
-        if isinstance(retorno_func, Retorno):
-            retorno_func = retorno_func.value
-
-        return_type = func[0]
-        if retorno_func is None:
-            if return_type != "":
-                raise Exception(f"Retorno tipo None não esperada para uma função \"{self.value}\" tipo void")
-        else:
-            if return_type != retorno_func[0]:
-                raise Exception(f"Retorno tipo {retorno_func[0]} não esperada para uma função \"{self.value}\" tipo {return_type}")
-        return retorno_func
-
-class Retorno:
-    def __init__(self, value):
-        self.value = value
-
-class Return(Node):
-    def Evaluate(self, st):
-        result = self.children[0].Evaluate(st)
-        return Retorno(result)
-
-class UnOp(Node):
-    def Evaluate(self, st):
-        result = self.children[0].Evaluate(st)
-        if self.value == MINUS:
-            if result[0] == INT:
-                return (INT, - result[1], False)
-            raise Exception(f"Tipo esperado: INT; Tipo recebido: {result[0]}")
-        if self.value == PLUS:
-            if result[0] == INT:
-                return (INT, result[1], False)
-            raise Exception(f"Tipo esperado: INT; Tipo recebido: {result[0]}")
-        if self.value == NOT:
-            if result[0] == BOOL:
-                return (BOOL, lower_bool(not BOOLEANS[result[1]]), False)
-            raise Exception(f"Tipo esperado: BOOL; Tipo recebido: {result[0]}")
-
-class BinOp(Node):
-    def Evaluate(self, st):
-        result0 = self.children[0].Evaluate(st)
-        result1 = self.children[1].Evaluate(st)
-        if self.value == MULT:
-            if result0[0] == INT and result1[0] == INT:
-                return (INT, result0[1] * result1[1], False)
-            raise Exception(f"Tipos esperados: INT e INT; Tipos recebidos: {result0[0]} e {result1[0]}")
-        if self.value == DIV:
-            if result0[0] == INT and result1[0] == INT:
-                return (INT, result0[1] // result1[1], False)
-            raise Exception(f"Tipos esperados: INT e INT; Tipos recebidos: {result0[0]} e {result1[0]}")
-        if self.value == MINUS:
-            if result0[0] == INT and result1[0] == INT:
-                return (INT, result0[1] - result1[1], False)
-            raise Exception(f"Tipos esperados: INT e INT; Tipos recebidos: {result0[0]} e {result1[0]}")
-        if self.value == PLUS:
-            if result0[0] == INT and result1[0] == INT:
-                return (INT, result0[1] + result1[1], False)
-            str0 = result0[1]
-            str1 = result1[1]
-            if result0[0] == BOOL:
-                str0 = str(result0[1]).lower()
-            if result1[0] == BOOL:
-                str1 = str(result1[1]).lower()
-            return (STR, f"{str0}{str1}", False)
-        if self.value == AND:
-            if result0[0] == BOOL and result1[0] == BOOL:
-                return (BOOL, lower_bool(BOOLEANS[result0[1]] and BOOLEANS[result1[1]]), False)
-            raise Exception(f"Tipos esperados: BOOL e BOOL; Tipos recebidos: {result0[0]} e {result1[0]}")
-        if self.value == OR:
-            if result0[0] == BOOL and result1[0] == BOOL:
-                return (BOOL, lower_bool(BOOLEANS[result0[1]] or BOOLEANS[result1[1]]), False)
-            raise Exception(f"Tipos esperados: BOOL e BOOL; Tipos recebidos: {result0[0]} e {result1[0]}")
-        if self.value == EQUAL:
-            if result0[0] == INT and result1[0] == INT or result0[0] == BOOL and result1[0] == BOOL or result0[0] == STR and result1[0] == STR:
-                return (BOOL, lower_bool(result0[1] == result1[1]), False)
-            raise Exception(f"Tipos esperados: INT e INT ou BOOL e BOOL ou STR e STR; Tipos recebidos: {result0[0]} e {result1[0]}")
-        if self.value == MORE:
-            if result0[0] == INT and result1[0] == INT or result0[0] == STR and result1[0] == STR:
-                return (BOOL, lower_bool(result0[1] > result1[1]), False)
-            raise Exception(f"Tipos esperados: INT e INT ou STR e STR; Tipos recebidos: {result0[0]} e {result1[0]}")
-        if self.value == LESS:
-            if result0[0] == INT and result1[0] == INT or result0[0] == STR and result1[0] == STR:
-                return (BOOL, lower_bool(result0[1] < result1[1]), False)
-            raise Exception(f"Tipos esperados: INT e INT ou STR e STR; Tipos recebidos: {result0[0], result0[1]} e {result1[0], result1[1]}")
-
 class Virar(Node):
     def Evaluate(self, st):
         coord = self.children[0].Evaluate(st)[1]
@@ -295,10 +187,6 @@ class Enquanto(Node):
 class Identifier(Node):
     def Evaluate(self, st):
         return st.getter(self.value)
-    
-class Print(Node):
-    def Evaluate(self, st):
-        print(self.children[0].Evaluate(st)[1])
 
 class Assignment(Node):
     def Evaluate(self, st):
@@ -316,33 +204,4 @@ class Block(Node):
                 result = child.Evaluate(newer_st)
             else:
                 result = child.Evaluate(new_st)
-            if type(result) is Retorno:
-                return result
         return None
-
-class If(Node):
-    def Evaluate(self, st):
-        if self.children[0].Evaluate(st)[0] != BOOL:
-            raise Exception(f"Condição para if (tipo BOOL) esperada; tipo recebido: {self.children[0].Evaluate(st)[0]}")
-        if BOOLEANS[self.children[0].Evaluate(st)[1]]:
-            result = self.children[1].Evaluate(st)
-            if type(result) is Retorno:
-                return result
-        else:
-            if len(self.children) == 3:
-                result = self.children[2].Evaluate(st)
-                if type(result) is Retorno:
-                    return result
-
-class While(Node):
-    def Evaluate(self, st):
-        if self.children[0].Evaluate(st)[0] != BOOL:
-            raise Exception(f"Condição para for (tipo BOOL) esperada; tipo recebido: {self.children[0].Evaluate(st)[0]}")
-        while BOOLEANS[self.children[0].Evaluate(st)[1]]:
-            result = self.children[1].Evaluate(st)
-            if type(result) is Retorno:
-                return result
-
-class Read(Node):
-    def Evaluate(self, st):
-        return (INT, int(input()), False)
--- tokens.py
+++ tokens.py
@@ -5,19 +5,12 @@ DESTINO = "destino"
 FECHADA = "fechada"
 ABERTA = "aberta"
 INT = "int"
-FUNC = "function"
-ARG = "argument"
 COMMA = ","
 COLON = ":"
 SEMICOLON = ";"
-ROTATORIA = "rotatoria"
-SAIDA = "saida"
 OPTION = "-"
-RETORNO = "retorno"
 ENQUANTO = "enquanto"
 SE = "se"
-MAIS = "mais"
-MENOS = "menos"
 MAIOR = "maior"
 MENOR = "menor"
 EM = "em"
@@ -25,7 +18,6 @@ METROS = "metros"
 VIRE = "vire"
 DIREITA = "direita"
 ESQUERDA = "esquerda"
-CHEGARA = "chegará"
 ESTA = "esta"
 CONTINUE = "continue"
 POR = "por"
@@ -40,15 +32,8 @@ KEYWORDS = {
     "Destino": DESTINO,
     "fechada": FECHADA,
     "aberta": ABERTA,
-    "Trajeto": FUNC,
-    "Ponto": ARG,
-    "Rotatória": ROTATORIA,
-    "Saída": SAIDA,
-    "retorno": RETORNO,
     "Enquanto": ENQUANTO,
     "Se": SE,
-    "mais": MAIS,
-    "menos": MENOS,
     "maior": MAIOR,
     "menor": MENOR,
     "em": EM,
@@ -56,7 +41,6 @@ KEYWORDS = {
     "Vire": VIRE,
     "direita": DIREITA,
     "esquerda": ESQUERDA,
-    "chegará": CHEGARA,
     "esta": ESTA,
     "Continue": CONTINUE,
     "por": POR,
@@ -69,51 +53,3 @@ TYPE = "type"
 INT = "int"
 BOOL = "bool"
 STR = "string"
-PLUS = "+"
-MINUS = "-"
-MULT = "*"
-DIV = "/"
-OR = "||"
-AND = "&&"
-NOT = "!"
-MORE = ">"
-LESS = "<"
-EQUAL = "=="
-COMMA = ","
-FOR = "for"
-IF = "if"
-ELSE = "else"
-READ = "read"
-OPENPAR = "("
-CLOSEPAR = ")"
-REC = "="
-IDEN = "identifier"
-PRINT = "print"
-NL = r"\n"
-OPENKEY = "{"
-CLOSEKEY = "}"
-FUNC = "func"
-RETURN = "return"
-EOF = ""
-
-# KEYWORDS = {
-#     "Println": PRINT,
-#     "for": FOR,
-#     "if": IF,
-#     "else": ELSE,
-#     "Scan": READ,
-#     "var": VAR,
-#     "func": FUNC,
-#     "return": RETURN
-# }
-
-TYPES = {
-    "int": TYPE,
-    "string": TYPE,
-    "bool": TYPE
-}
-
-BOOLEANS = {
-    "false": False,
-    "true": True
-}
